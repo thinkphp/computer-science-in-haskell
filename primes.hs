5b25c147d39c122c7b701082b4b436a69c9af50d
Adrian Statescu <mergesortv@gmail.com>
2017-01-01 09:19:02 -0800

    display primes

---
+++ primes.hs
@@ -0,0 +1,16 @@
+factors :: Int -> [ Int ]
+
+factors n = [ x | x <- [1..n], n `mod` x == 0 ]
+
+prime :: Int -> Bool
+
+prime n = factors n == [1,n]
+
+primes :: Int -> [ Int ]
+
+primes n = [x| x <- [2..n], prime x]
+
+main = do putStrLn "Give me a number"
+          n <- readLn :: IO Int 
+          print $ prime n
+          print $ primes 100
