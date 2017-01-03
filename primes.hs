bb5d8af98425252d84a8b63dc25a31005f345d73
Adrian Statescu <mergesortv@gmail.com>
2017-01-03 09:42:52 -0800

    explanations

--- primes.hs
+++ primes.hs
@@ -1,3 +1,7 @@
+-- List comprehensions can also use logical expressions called GUARDs to filter the values produced by earlier generators.
+-- If a guard is True, then the current values are retained, and, if it is False, then they are discarded.
+-- For example, the comprehension [x|x<-[1..10], even x] produces the list [2,4,6,8,10] of all even numbers from the list [1..10]
+
 factors :: Int -> [ Int ]
 
 factors n = [ x | x <- [1..n], n `mod` x == 0 ]
